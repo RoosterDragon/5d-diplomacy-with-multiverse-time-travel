ef5b2d9caef149d81300edc61290112cd4c34beb
RoosterDragon <RoosterDragon@outlook.com>
2025-03-19 18:54:23 +0000

    Add ability to load saved game files.
    - There is an existing "Download game as JSON" button in the UI, reuse this but adjust the save format.
    - Add an endpoint that accepts a upload of a previously saved JSON file. We create a fresh world and replay the orders over it.
    - Add a new UI to load such a previously saved JSON file via this new endpoint, similar to creating a game.
    
    Given the pragmatic reality that there is no single persistent online server, sharing games or analyzing them is somewhat difficult. By introducing the ability to load saved games, users can at least import interesting games into their local servers.

--- server/Repositories/WorldRepository.cs
+++ server/Repositories/WorldRepository.cs
@@ -5,16 +5,23 @@ using Entities;
 using Enums;
 using Exceptions;
 using Factories;
+using Mappers;
 using Microsoft.EntityFrameworkCore;
 
 namespace Repositories;
 
-public class WorldRepository(ILogger<WorldRepository> logger, GameContext context, RegionMapFactory regionMapFactory, DefaultWorldFactory defaultWorldFactory)
+public class WorldRepository(
+    ILogger<WorldRepository> logger,
+    GameContext context,
+    RegionMapFactory regionMapFactory,
+    DefaultWorldFactory defaultWorldFactory,
+    ModelMapper modelMapper)
 {
     private readonly ILogger<WorldRepository> logger = logger;
     private readonly GameContext context = context;
     private readonly RegionMapFactory regionMapFactory = regionMapFactory;
     private readonly DefaultWorldFactory defaultWorldFactory = defaultWorldFactory;
+    private readonly ModelMapper modelMapper = modelMapper;
 
     public async Task<World> GetWorld(int gameId)
     {
@@ -39,7 +46,7 @@ public class WorldRepository(ILogger<WorldRepository> logger, GameContext contex
         return iteration;
     }
 
-    public async Task AddOrders(int gameId, Nation[] players, List<Order> orders)
+    public async Task AddOrders(int gameId, Nation[] players, IEnumerable<Models.Order> orders)
     {
         logger.LogInformation("Submitting orders for game {GameId}", gameId);
 
@@ -56,7 +63,7 @@ public class WorldRepository(ILogger<WorldRepository> logger, GameContext contex
         game.PlayersSubmitted = [.. game.PlayersSubmitted, .. players];
 
         var world = await GetWorldInternal(gameId, true);
-        world.Orders.AddRange(orders);
+        world.Orders.AddRange(orders.Select(o => modelMapper.MapOrder(world, o)));
 
         if (world.LivingPlayers.Count <= game.PlayersSubmitted.Count)
         {
@@ -75,15 +82,42 @@ public class WorldRepository(ILogger<WorldRepository> logger, GameContext contex
         await transaction.CommitAsync();
     }
 
+    public async Task AddOrdersForMultipleIterations(int gameId, int targetIteration, Func<World, IEnumerable<Models.Order>> getOrdersForIteration)
+    {
+        logger.LogInformation("Submitting orders for multiple iterations for game {GameId}", gameId);
+
+        await using var transaction = await context.Database.BeginTransactionAsync(IsolationLevel.Serializable);
+        var game = await context.Games.FindAsync(gameId)
+            ?? throw new GameNotFoundException();
+
+        game.PlayersSubmitted = [];
+
+        var world = await GetWorldInternal(gameId, true);
+
+        while (world.Iteration < targetIteration)
+        {
+            var orders = getOrdersForIteration(world);
+            world.Orders.AddRange(orders.Select(o => modelMapper.MapOrder(world, o)));
+
+            var adjudicator = new Adjudicator(world, game.HasStrictAdjacencies, regionMapFactory, defaultWorldFactory);
+            adjudicator.Adjudicate();
+            world.Iteration++;
+        }
+
+        await context.SaveChangesAsync();
+        await transaction.CommitAsync();
+    }
+
     private async Task<World> GetWorldInternal(int gameId, bool shouldTrackEntities)
     {
         logger.LogInformation("Querying world for game {GameId}", gameId);
 
         // Should be called within a serializable transaction.
+        // Orders must be returned in the same order they were submitted, required for save/load to function correctly.
         var worldQuery = context.Worlds
             .Include(w => w.Boards).ThenInclude(b => b.Centres)
             .Include(w => w.Boards).ThenInclude(b => b.Units)
-            .Include(w => w.Orders).ThenInclude(o => o.Unit)
+            .Include(w => w.Orders.OrderBy(o => o.Id)).ThenInclude(o => o.Unit)
             .AsSplitQuery();
 
         if (!shouldTrackEntities)
